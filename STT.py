ecf2b5b03da33da03f4b012beef86f32684dbf80
whr-a <2472356642@qq.com>
2024-05-23 08:59:59 +0800

    fix STT.py

--- STT.py
+++ STT.py
@@ -1,22 +1,11 @@
 import os
 import soundfile
-from espnet_model_zoo.downloader import ModelDownloader
-from espnet2.bin.asr_inference import Speech2Text
+import nemo.collections.asr as nemo_asr
+asr_model = nemo_asr.models.EncDecRNNTBPEModel.from_pretrained("nvidia/stt_en_conformer_transducer_xlarge")
 
-d = ModelDownloader()
 
 def transcribe_audio_files(audio_dir, output_file):
     # Load the pretrained STT model
-    speech2text = Speech2Text(
-        **d.download_and_unpack(task="asr", corpus="wsj"),
-        maxlenratio=0.0,
-        minlenratio=0.0,
-        beam_size=20,
-        ctc_weight=0.3,
-        lm_weight=0.5,
-        penalty=0.0,
-        nbest=1
-    )
 
     # Open the output file for writing
     with open(output_file, "w", encoding="utf-8") as f:
@@ -26,14 +15,8 @@ def transcribe_audio_files(audio_dir, output_file):
             audio_path = os.path.join(audio_dir, f"0_{i}_waveglow.wav")
             print("Transcribing:", audio_path)
 
-            # Read the audio file
-            speech, rate = soundfile.read(audio_path)
-
-            # Perform speech-to-text transcription
-            nbests = speech2text(speech)
-
             # Write the transcription result to the output file
-            text, *_ = nbests[0]
+            text, *_ = asr_model.transcribe([audio_path])[0]
             f.write(text + "\n")
 
     print("Transcription complete. Results saved to:", output_file)
@@ -44,3 +27,4 @@ audio_directory = "/home/whr-a/TTS/FastSpeech/results"
 output_text_file = "/home/whr-a/TTS/STTresult/result.txt"
 # 调用函数进行语音转文本
 transcribe_audio_files(audio_directory, output_text_file)
+# Average Word Error Rate (WER): 0.08063332113898879
